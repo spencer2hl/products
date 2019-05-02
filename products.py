d2c2e4ffb5fa08dd2f0cb282e6fe660317f5c608
spencer2hl <frost0901@outlook.com>
2019-05-02 13:17:34 -0500

    add read file

--- products.py
+++ products.py
@@ -1,19 +1,30 @@
+# 读取档案
 products = []
+with open('products.txt', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品, 价格' in line:
+			continue		
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products) 
+
+#让使用者输入
 while True:
 	name = input('请输入商品名称：')
 	if name == 'q': 
 		break
 	price = input('请输入商品价格：')
 	price = int(price)
-	products.append([name, price])
 print(products)
 
+#印出所有购买记录
 for p in products:
 	print(p[0], '的价格', p[1])
 
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
+#写入档案
 with open('products.txt', 'w', encoding = 'utf-8') as f:
 	f. write('商品, 价格\n')
 	for p in products:
