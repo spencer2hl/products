355de291b943224353a8c41e186632ba5caf3b6d
spencer2hl <frost0901@outlook.com>
2019-05-02 12:27:58 -0500

    add header

--- products.py
+++ products.py
@@ -4,10 +4,25 @@ while True:
 	if name == 'q': 
 		break
 	price = input('请输入商品价格：')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
-
 for p in products:
 	print(p[0], '的价格', p[1])
+
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+with open('products.txt', 'w', encoding = 'utf-8') as f:
+	f. write('商品, 价格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # ‘\n’ 换行
+
+
+
+
+
+
+
 
